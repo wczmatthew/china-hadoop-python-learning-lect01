4040780376f6a6b1283bf6d5bd732854979e76bf
wczblackman <green0072004@gmail.com>
2018-06-22 15:22:50 +0800

    分支判断 人民币 美元 其他，输出结果

--- currency_converter_v2.0.py
+++ currency_converter_v2.0.py
@@ -15,7 +15,31 @@ currency_str_value = input('请输入带单位的货币金额:')
 # 获取货币单位
 unit = currency_str_value[-3:]
 
-print(unit)
+if unit == 'CNY':
+    # 输入的是人民币
+    rmb_str_value = currency_str_value[:-3]
+    # 将字符串转换为数字
+    rmb_value = eval(rmb_str_value)
+    # 汇率转换
+    usd_value = rmb_value / USD_VS_RMB
+
+    # 输入结果
+    print('美元(USD)金额是：', usd_value)
+elif unit == 'USD':
+    # 输入的是美元
+    usd_str_value = currency_str_value[:-3]
+    # 将字符串转换为数字
+    usd_value = eval(usd_str_value)
+    # 汇率计算
+    rmb_value = usd_value * USD_VS_RMB
+
+    # 输出结果
+    print('人民币(CNY)金额是：', rmb_value)
+else:
+    # 其他情况
+    print('目前版本尚不支持该种货币')
+
+
 # # 将字符串转换为数字
 # rmb_value = eval(rmb_str_value)
 #
